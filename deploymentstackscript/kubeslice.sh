1be5a582df5dfa8f1b6f9a6b4e9f80a0103177f5
Juan Coto <jcotsan@gmail.com>
2023-10-31 10:11:09 -0600

    adding var and fixing k8s_info reference

--- deploymentstackscript/kubeslice.sh
+++ deploymentstackscript/kubeslice.sh
@@ -8,6 +8,21 @@ trap "cleanup $? $LINENO" EXIT
 #<UDF name="disable_root" label="Disable root access over SSH?" oneOf="Yes,No" default="No">
 #<UDF name="pubkey" label="The SSH Public Key that will be used to access the Linode (Recommended)" default="">
 
+##Controller kubernetes cluster
+#<UDF name="controller_cluster_label" label="Controller cluster label">
+#<UDF name="controller_cluster_dc" Label="The datacenter for your Controller cluster" oneOf="ap-west,ca-central,ap-southeast,us-iad,us-ord,fr-par,us-sea,br-gru,nl-ams,se-sto,in-maa,jp-osa,it-mil,us-mia,id-cgk,us-lax,us-central,us-west,us-southeast,us-east,eu-west,ap-south,eu-central", default="us-sea" />>
+#<UDF name="controller_cluster_version" label="Controller kubernetes version" oneOf="1.26,1.27", default="1.27">
+#<UDF name="controller_cluster_node_plan" Label="The plan for your Controller nodes" oneOf="g6-dedicated-2,g6-dedicated-4,g6-dedicated-8,g6-dedicated-16,g6-dedicated-32,g6-dedicated-48,g6-dedicated-50,g6-dedicated-56,g6-dedicated-64", default="g6-dedicated-8">
+#<UDF name="controller_cluster_nodes" label="Number of nodes for Controller cluster">
+
+
+##Worker kubernetes cluster
+#<UDF name="worker_cluster_label" label="Worker cluster label">
+#<UDF name="worker_cluster_dc" Label="The datacenter for your Worker cluster" oneOf="ap-west,ca-central,ap-southeast,us-iad,us-ord,fr-par,us-sea,br-gru,nl-ams,se-sto,in-maa,jp-osa,it-mil,us-mia,id-cgk,us-lax,us-central,us-west,us-southeast,us-east,eu-west,ap-south,eu-central", default="us-sea" />>
+#<UDF name="worker_cluster_version" label="Worker kubernetes version" oneOf="1.26,1.27", default="1.27">
+#<UDF name="worker_cluster_node_plan" Label="The plan for your Worker nodes" oneOf="g6-dedicated-2,g6-dedicated-4,g6-dedicated-8,g6-dedicated-16,g6-dedicated-32,g6-dedicated-48,g6-dedicated-50,g6-dedicated-56,g6-dedicated-64", default="g6-dedicated-8">
+#<UDF name="worker_cluster_nodes" label="Number of nodes for Worker cluster">
+
 ## Kubeslice project
 #<UDF name="kubeslice_project" label="Kubeslice project name">
 
@@ -58,6 +73,50 @@ function udf {
     echo "pubkey: ${PUBKEY}" >> ${group_vars};
   else echo "No pubkey entered";
   fi
+
+  #LKE vars
+
+  if [[ -n ${CONTROLLER_CLUSTER_LABEL} ]]; then
+    echo "controller_cluster_label: ${CONTROLLER_CLUSTER_LABEL}" >> ${group_vars};
+  fi
+
+  if [[ -n ${CONTROLLER_CLUSTER_DC} ]]; then
+    echo "controller_cluster_dc: ${CONTROLLER_CLUSTER_DC}" >> ${group_vars};
+  fi
+
+  if [[ -n ${CONTROLLER_CLUSTER_VERSION} ]]; then
+    echo "controller_cluster_version: ${CONTROLLER_CLUSTER_VERSION}" >> ${group_vars};
+  fi
+
+  if [[ -n ${CONTROLLER_CLUSTER_NODE_PLAN} ]]; then
+    echo "controller_cluster_node_plan: ${CONTROLLER_CLUSTER_NODE_PLAN}" >> ${group_vars};
+  fi
+
+  if [[ -n ${CONTROLLER_CLUSTER_NODES} ]]; then
+    echo "controller_cluster_nodes: ${CONTROLLER_CLUSTER_NODES}" >> ${group_vars};
+  fi
+
+
+  if [[ -n ${WORKER_CLUSTER_LABEL} ]]; then
+    echo "Worker_cluster_label: ${WORKER_CLUSTER_LABEL}" >> ${group_vars};
+  fi
+
+  if [[ -n ${WORKER_CLUSTER_DC} ]]; then
+    echo "Worker_cluster_dc: ${WORKER_CLUSTER_DC}" >> ${group_vars};
+  fi
+
+  if [[ -n ${WORKER_CLUSTER_VERSION} ]]; then
+    echo "Worker_cluster_version: ${WORKER_CLUSTER_VERSION}" >> ${group_vars};
+  fi
+
+  if [[ -n ${WORKER_CLUSTER_NODE_PLAN} ]]; then
+    echo "worker_cluster_node_plan: ${WORKER_CLUSTER_NODE_PLAN}" >> ${group_vars};
+  fi
+
+  if [[ -n ${WORKER_CLUSTER_NODES} ]]; then
+    echo "Worker_cluster_nodes: ${WORKER_CLUSTER_NODES}" >> ${group_vars};
+  fi
+
 
   # Kubeslice vars
 
