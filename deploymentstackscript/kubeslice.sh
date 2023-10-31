cf3d6d5d48c92ebb8b95e5a55fad0663ff03cfd0
Juan Coto <jcotsan@gmail.com>
2023-10-31 10:50:32 -0600

    fixing deploy script

--- deploymentstackscript/kubeslice.sh
+++ deploymentstackscript/kubeslice.sh
@@ -16,7 +16,7 @@ trap "cleanup $? $LINENO" EXIT
 #<UDF name="controller_cluster_label" label="Controller cluster label">
 #<UDF name="controller_cluster_dc" Label="The datacenter for your Controller cluster" oneOf="ap-west,ca-central,ap-southeast,us-iad,us-ord,fr-par,us-sea,br-gru,nl-ams,se-sto,in-maa,jp-osa,it-mil,us-mia,id-cgk,us-lax,us-central,us-west,us-southeast,us-east,eu-west,ap-south,eu-central" default="us-sea">
 #<UDF name="controller_cluster_version" label="Controller kubernetes version" oneOf="1.26,1.27" default="1.27">
-#<UDF name="controller_cluster_node_plan" Label="The plan for your Controller nodes" oneOf="g6-dedicated-2,g6-dedicated-4,g6-dedicated-8,g6-dedicated-16,g6-dedicated-32,g6-dedicated-48,g6-dedicated-50,g6-dedicated-56,g6-dedicated-64" default="g6-dedicated-8">
+#<UDF name="controller_cluster_node_plan" Label="The plan for your Controller nodes" oneOf="g6-dedicated-2,g6-dedicated-4,g6-dedicated-8,g6-dedicated-16,g6-dedicated-32,g6-dedicated-48,g6-dedicated-50,g6-dedicated-56,g6-dedicated-64" default="g6-dedicated-4">
 #<UDF name="controller_cluster_nodes" label="Number of nodes for Controller cluster">
 
 
@@ -24,7 +24,7 @@ trap "cleanup $? $LINENO" EXIT
 #<UDF name="worker_cluster_label" label="Worker cluster label">
 #<UDF name="worker_cluster_dc" Label="The datacenter for your Worker cluster" oneOf="ap-west,ca-central,ap-southeast,us-iad,us-ord,fr-par,us-sea,br-gru,nl-ams,se-sto,in-maa,jp-osa,it-mil,us-mia,id-cgk,us-lax,us-central,us-west,us-southeast,us-east,eu-west,ap-south,eu-central" default="us-sea" />
 #<UDF name="worker_cluster_version" label="Worker kubernetes version" oneOf="1.26,1.27" default="1.27">
-#<UDF name="worker_cluster_node_plan" Label="The plan for your Worker nodes" oneOf="g6-dedicated-2,g6-dedicated-4,g6-dedicated-8,g6-dedicated-16,g6-dedicated-32,g6-dedicated-48,g6-dedicated-50,g6-dedicated-56,g6-dedicated-64" default="g6-dedicated-8">
+#<UDF name="worker_cluster_node_plan" Label="The plan for your Worker nodes" oneOf="g6-dedicated-2,g6-dedicated-4,g6-dedicated-8,g6-dedicated-16,g6-dedicated-32,g6-dedicated-48,g6-dedicated-50,g6-dedicated-56,g6-dedicated-64" default="g6-dedicated-4">
 #<UDF name="worker_cluster_nodes" label="Number of nodes for Worker cluster">
 
 ## Kubeslice project
